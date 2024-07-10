ada66b45d01cce53e8f32a0be2dbd5e469b657d7
Martincho03 <martin.brynkier@gmail.com>
2024-07-09 21:33:16 -0300

    reentrega2

--- Proyecto.js
+++ Proyecto.js
@@ -1,6 +1,88 @@
 let promedios = [];
 
+let juegos = [
+    {
+        nombre: "League of Legends",
+        promedios: [],
+    },
+    {
+        nombre: "Dota 2",
+        promedios: [],
+    },
+    {
+        nombre: "Counter Strike 2",
+        promedios: [],
+    },
+    {
+        nombre: "Valorant",
+        promedios: [],
+    },
+];
+
+function obtenerJuego() {
+    let texto = "";
+    let i;
+    for (i = 0; i < juegos.length; i++) {
+        texto += "* " + juegos[i].nombre + "\n";
+    }
+
+    let juegoSeleccionado = prompt(
+        texto + "* Resultados\nSelecciona una opción"
+    );
+
+    juegoSeleccionado = juegoSeleccionado.replaceAll(" ", "").toLowerCase();
+
+    if (juegoSeleccionado == "resultados") {
+        return "R";
+    }
+
+    let juegoEncontrado = juegos.find(
+        (juego) =>
+            juego.nombre.replaceAll(" ", "").toLowerCase() == juegoSeleccionado
+    );
+
+    if (juegoEncontrado) {
+        return juegos.indexOf(juegoEncontrado);
+    } else {
+        return obtenerJuego();
+    }
+}
+
+function mostrarResultados() {
+    let texto = "Tus promedios son: \n";
+
+    for (let i = 0; i < juegos.length; i++) {
+        texto += juegos[i].nombre + ": ";
+
+        if (juegos[i].promedios.length > 0) {
+            texto += juegos[i].promedios;
+            let buenosPromedios = juegos[i].promedios.filter(
+                (promedio) => promedio > 2
+            );
+            if (buenosPromedios) {
+                texto +=
+                    " (Cantidad de buenos promedios: " +
+                    buenosPromedios.length +
+                    ")";
+            }
+        } else {
+            texto += "No hay datos cargados";
+        }
+
+        texto += "\n";
+    }
+    alert(texto);
+    main();
+}
+
 function main() {
+    let juegoSeleccionado = obtenerJuego();
+
+    if (juegoSeleccionado == "R") {
+        mostrarResultados();
+        return;
+    }
+
     const partidasIngresadas = prompt(
         "Ingrese cuantas partidas desea evaluar (min:2 / max:6)"
     );
@@ -14,19 +96,11 @@ function main() {
         }
         const calculoDeKDA = sumatoriadepartidas / partidasIngresadas;
 
-        promedios.push(calculoDeKDA);
+        juegos[juegoSeleccionado].promedios.push(calculoDeKDA);
 
-        let respuesta = prompt(
-            "¿Quiere calcular otro promedio?(S/N)"
-        ).toUpperCase();
+        let respuesta = prompt("¿Volver al menu? (S/N)").toUpperCase();
         if (respuesta == "S") {
             main();
-        } else {
-            for (let i = 0; i < promedios.length; i++) {
-                alert(
-                    "El promedio numero " + (i + 1) + " es = " + promedios[i]
-                );
-            }
         }
     } else {
         alert(
@@ -34,19 +108,4 @@ function main() {
         );
     }
 }
-
-function caca() {
-    const respuesta = prompt(
-        "Queres que te siga preguntando? (S/N)"
-    ).toUpperCase();
-
-    if (respuesta == "S") {
-        caca();
-    } else {
-        for (let i; i < promedios.length; i++) {
-            alert(promedios[i]);
-        }
-    }
-}
-
 main();
